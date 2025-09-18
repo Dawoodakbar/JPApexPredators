85a884aa7499c3af3ad920f55a7421fa7a5c2a49
Dawood <dawood.3techsoft@gmail.com>
2025-09-18 16:20:57 +0500

    Add enum for dinosaure types, and added the computed property for it to get the type based on json

--- JPApexPredators/ApexPredator.swift
+++ JPApexPredators/ApexPredator.swift
@@ -4,21 +4,43 @@
 //
 //  Created by dawood on 9/17/25.
 //
+import SwiftUI
 
-struct ApexPredator: Decodable {
+struct ApexPredator: Decodable, Identifiable {
     let id: Int
     let name: String
-    let type: String
+    let type: ApType
     let latitude: Double
     let longitude: Double
     let movies: [String]
-    let movieScene: [MovieScene]
+    let movieScenes: [MovieScene]
     let link: String
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
     
     struct MovieScene: Decodable {
         let id: Int
         let movie: String
         let sceneDescription: String
+    }
+    
+    enum ApType: String, Decodable {
+        case land
+        case air
+        case sea
+        
+        var background: Color {
+            switch self {
+            case .land:
+                    .brown
+            case .air:
+                    .teal
+            case .sea:
+                    .blue
+            }
+        }
     }
 }
 
